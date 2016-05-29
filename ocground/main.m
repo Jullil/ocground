e942f715e8bff4521f4f41244b260f9e774c30be
Julia Loikova <julinary@gmail.com>
2016-05-29 22:43:37 +0200

    Initial Commit

---
+++ ocground/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ocground
+//
+//  Created by Julia Loykova on 29/05/16.
+//  Copyright © 2016 Julia Loykova. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
